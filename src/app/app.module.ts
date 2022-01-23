c77a5c33944ae8a6d343f31975f787f1e3b09981
ELASRI MOHAMMED <1mohamedelasri@gmail.com>
2022-01-23 17:40:01 +0100

    Setup project architecture + Admin LTE dashboard integration

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './layout/nav-bar/nav-bar.component';
+import { SideBarComponent } from './layout/side-bar/side-bar.component';
+import { DashboardComponent } from './layout/dashboard/dashboard.component';
+import { FooterComponent } from './layout/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SideBarComponent,
+    DashboardComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
