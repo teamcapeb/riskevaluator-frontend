59f54326545ff0caef43ba9ae8f906d2bc64ea48
AdamMadide <AdamMadide>
2022-02-01 14:46:49 +0100

    [IMP] Reponse service base

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  apiUrl: 'https://7733421e-9489-4c1f-91b2-b2839b432fd8.mock.pstmn.io/api',
 };
 
 /*
