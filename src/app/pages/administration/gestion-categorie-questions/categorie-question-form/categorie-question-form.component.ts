24462081378b35396bc2560e4387e46f80ecaf04
AdamMadide <AdamMadide>
2022-02-08 10:22:56 +0100

    liste de categorie finit

--- src/app/pages/administration/gestion-categorie-questions/categorie-question-form/categorie-question-form.component.ts
+++ src/app/pages/administration/gestion-categorie-questions/categorie-question-form/categorie-question-form.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import CategorieQuestion from '@/objects/CategorieQuestion';
+import { Component, EventEmitter, Input, OnInit, Output, ViewChild } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import { ModalDismissReasons, NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-categorie-question-form',
@@ -7,9 +10,52 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategorieQuestionFormComponent implements OnInit {
 
-  constructor() { }
+  closeResult = '';
+  public title: string;
+  public registerForm: FormGroup;
+  @Input() CategorieQuestion: CategorieQuestion;
+  @ViewChild('modalContentCreateOrUpdate') modalContentCreateOrUpdate: any;
+  @ViewChild('modalDelete') modalDelete: any;
+  @Output() onConfirmation: EventEmitter<any> = new EventEmitter();
+
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
+    this.registerForm = new FormGroup({
+      categorieQuestionName: new FormControl()
+    });
+  }
+
+
+
+  public open(action: string) {
+    let content = null;
+    if(action === 'add' || action === 'update'){
+      content = this.modalContentCreateOrUpdate;
+      this.title = action === 'add' ? 'Ajouter' : 'Modifier';
+    }else if(action === 'delete'){
+      content = this.modalDelete;
+      this.title = 'Supprimer';
+    }
+    this.modalService.open(content, {animation: true, centered: true}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+      console.log(result)
+      if(result === 'Valider'){
+        this.onConfirmation.emit({data: this.CategorieQuestion, action: action});
+      }
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
+    }
   }
 
 }
