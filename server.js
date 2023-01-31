1cfcf8d6b7dd7a02dfb747b531f4fb391399a630
Karim Aboud <online.karim1@gmail.com>
2023-01-31 03:10:25 +0100

    fix

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 const app = express();
-app.use(express.static(__dirname + '/dist/app'));
-app.get('/*', function(req,res) {
-  res.sendFile(path.join(__dirname+
-    '/dist/app/index.html'));});
+
+app.use(express.static(__dirname + "/dist/app"));
+
+app.get("/*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/dist/app/index.html"));
+});
+
 app.listen(process.env.PORT || 8080);
