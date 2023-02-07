51bcc6cfc7f61ee681c8da1eb61b83458891d250
Karim Aboud <online.karim1@gmail.com>
2023-02-07 23:04:41 +0100

    &score par metier chart

--- src/app/pages/administration/statistics/statistics.component.ts
+++ src/app/pages/administration/statistics/statistics.component.ts
@@ -1,23 +1,23 @@
-import { EvaluationApiService } from '@services/serviceEvaluation/evaluation-api.service';
-import { CategorieQuestionService } from '@services/serviceCategorieQuestion/categorie-question.service';
-import { QuestionnaireService } from '@services/serviceQuestionnaire/questionnaire.service';
-import { IEntreprise } from '@/interfaces/IEntreprise';
-import { IMetier } from '@/interfaces/IMetier';
-import IQuestionnaire from '@/interfaces/IQuestionnaire';
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-import { MetierService } from '@services/serviceMetier/metier.service';
-import { EntrepriseService } from '@services/serviceEntreprise/entreprise.service';
-import ICategorieQuestion from '@/interfaces/ICategorieQuestion';
-import IEvaluation from '@/interfaces/IEvaluation';
-import { TuiContextWithImplicit, tuiSum } from '@taiga-ui/cdk';
-import { tuiFormatNumber } from '@taiga-ui/core';
+import { EvaluationApiService } from "@services/serviceEvaluation/evaluation-api.service";
+import { CategorieQuestionService } from "@services/serviceCategorieQuestion/categorie-question.service";
+import { QuestionnaireService } from "@services/serviceQuestionnaire/questionnaire.service";
+import { IEntreprise } from "@/interfaces/IEntreprise";
+import { IMetier } from "@/interfaces/IMetier";
+import IQuestionnaire from "@/interfaces/IQuestionnaire";
+import { ChangeDetectionStrategy, ChangeDetectorRef, Component, OnInit } from "@angular/core";
+import { FormControl, FormGroup } from "@angular/forms";
+import { MetierService } from "@services/serviceMetier/metier.service";
+import { EntrepriseService } from "@services/serviceEntreprise/entreprise.service";
+import ICategorieQuestion from "@/interfaces/ICategorieQuestion";
+import IEvaluation from "@/interfaces/IEvaluation";
+import { TuiContextWithImplicit } from "@taiga-ui/cdk";
+import { MetierScoreProjectionResponse } from "@/objects/MetierScoreProjectionResponse";
 
 @Component({
-  selector: 'app-statistics',
-  templateUrl: './statistics.component.html',
-  styleUrls: ['./statistics.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
+  selector: "app-statistics",
+  templateUrl: "./statistics.component.html",
+  styleUrls: ["./statistics.component.scss"],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class StatisticsComponent implements OnInit {
   public isExpanded1 = false;
@@ -33,25 +33,25 @@ export class StatisticsComponent implements OnInit {
   public expand3Counter = 0;
   public expand4Counter = 0;
 
-  entrepriseControl = new FormControl('');
-  metierControl = new FormControl('');
-  questionnaireControl = new FormControl('');
+  entrepriseControl = new FormControl("");
+  metierControl = new FormControl("");
+  questionnaireControl = new FormControl("");
 
   allEvaluations: IEvaluation[];
-  allEntreprises : IEntreprise[];
-  allMetiers : IMetier[];
-  allQuestionnaires : IQuestionnaire[];
+  allEntreprises: IEntreprise[];
+  allMetiers: IMetier[];
+  allQuestionnaires: IQuestionnaire[];
   allCategories: ICategorieQuestion[];
   allCategoriesLibelles: string[];
 
-  filteredEntreprises : IEntreprise[] = [];
+  filteredEntreprises: IEntreprise[] = [];
   filteredEvaluations: IEvaluation[] = [];
-  filteredMetiers : IMetier[] = [];
-  filteredQuestionnaires : IQuestionnaire[] = [];
+  filteredMetiers: IMetier[] = [];
+  filteredQuestionnaires: IQuestionnaire[] = [];
 
   range = new FormGroup({
     start: new FormControl(null),
-    end: new FormControl(null),
+    end: new FormControl(null)
   });
 
   petitesEntreprises: IEntreprise[] = [];
@@ -67,30 +67,37 @@ export class StatisticsComponent implements OnInit {
 
   value: number[][] = [];
 
-  readonly appearances = ['onDark', 'error'];
-  appearance = 'onDark';
-  readonly hint = ({$implicit}: TuiContextWithImplicit<number>): string => {
-    let result = '';
+  scoresMetiers: number [][] = [];
+
+  metierNames: string[];
+
+
+  readonly appearances = ["onDark", "error"];
+  appearance = "onDark";
+  readonly hint = ({ $implicit }: TuiContextWithImplicit<number>): string => {
+    let result = "";
     this.value.forEach((set) => {
-      result += set[$implicit] + '\n';
+      result += set[$implicit] + "\n";
     });
     console.log(result);
     return result.trim();
   };
+
 
   constructor(private evaluationService: EvaluationApiService,
               private entrepriseService: EntrepriseService,
               private metierService: MetierService,
               private questionnaireService: QuestionnaireService,
               private categorieQuestionService: CategorieQuestionService,
-              private cdr: ChangeDetectorRef) { }
+              private cdr: ChangeDetectorRef) {
+  }
 
   ngOnInit(): void {
     this.evaluationService.getAll().subscribe((res) => {
       this.allEvaluations = res;
       this.filteredEvaluations = res;
       this.cdr.detectChanges();
-    })
+    });
     this.entrepriseService.getAll().subscribe((res) => {
       this.allEntreprises = this.sortEntreprises(res);
       this.filteredEntreprises = [...this.allEntreprises];
@@ -113,6 +120,15 @@ export class StatisticsComponent implements OnInit {
         return categorie.libelle;
       }));
     });
+
+    //this.metierService.getScoreParMetier()
+    //    .subscribe((response: MetierScoreProjectionResponse[]) => {
+    //      this.metierNames = response.map(item => item.nomMetier);
+    //      this.scoresMetiers.push(response.map(item => item.scoreMoyen));
+    //      console.log(this.scoresMetiers);
+    //      this.cdr.detectChanges();
+    //    });
+
   }
 
   sortEntreprises(entreprises: IEntreprise[]): IEntreprise[] {
@@ -160,7 +176,7 @@ export class StatisticsComponent implements OnInit {
   selectEntreprise(entreprise: IEntreprise, selected: boolean) {
     if (selected) {
       if (this.entrepriseControl.value.length == 1) {
-        this.filteredEntreprises = []
+        this.filteredEntreprises = [];
       }
       this.allEntreprises.forEach(etp => {
         this.entrepriseControl.value.forEach((nom: string) => {
@@ -172,7 +188,7 @@ export class StatisticsComponent implements OnInit {
       this.updateFilteredMetiersByEntreprises();
       this.updateFilteredEvaluationsByEntreprises();
     } else {
-      this.filteredEntreprises.forEach((element,index) => {
+      this.filteredEntreprises.forEach((element, index) => {
         if (element.noSiret == entreprise.noSiret) {
           this.filteredEntreprises.splice(index, 1);
           this.updateFilteredMetiersByEntreprises();
@@ -199,9 +215,8 @@ export class StatisticsComponent implements OnInit {
         });
       });
       this.updateFilteredEntreprisesByMetiers();
-    }
-    else {
-      this.filteredMetiers.forEach((element,index) => {
+    } else {
+      this.filteredMetiers.forEach((element, index) => {
         if (element.nomMetier == metier.nomMetier) {
           this.filteredMetiers.splice(index, 1);
           this.updateFilteredEntreprisesByMetiers();
@@ -223,11 +238,9 @@ export class StatisticsComponent implements OnInit {
     entreprises.forEach((etp) => {
       if (etp.effectif <= 5) {
         this.petitesEntreprises.push(etp);
-      }
-      else if (etp.effectif >= 6 && etp.effectif <= 10) {
+      } else if (etp.effectif >= 6 && etp.effectif <= 10) {
         this.moyennesEntreprises.push(etp);
-      }
-      else if (etp.effectif > 10) {
+      } else if (etp.effectif > 10) {
         this.grandesEntreprises.push(etp);
       }
     });
@@ -242,18 +255,18 @@ export class StatisticsComponent implements OnInit {
 
     let evalsEntreprise: IEvaluation[] = [];
 
-    let dates : string[] = [];
+    let dates: string[] = [];
 
     this.petitesEntreprises.forEach((etp) => {
       sumScorePetites += etp.evaluations.map((evl) => {
         // TODO
-        return evl.scoreGeneraleEvaluation
+        return evl.scoreGeneraleEvaluation;
       }).reduce((total, num) => total + num, 0);
     });
     this.moyennesEntreprises.forEach((etp) => {
       sumScoreMoyennes += etp.evaluations.map((evl) => {
         // TODO
-        return evl.scoreGeneraleEvaluation
+        return evl.scoreGeneraleEvaluation;
       }).reduce((total, num) => total + num, 0);
     });
     this.grandesEntreprises.forEach((etp) => {
@@ -270,7 +283,7 @@ export class StatisticsComponent implements OnInit {
           console.log(mostRecentDate);
         });
 
-        return evl.scoreGeneraleEvaluation
+        return evl.scoreGeneraleEvaluation;
       }).reduce((total, num) => total + num, 0);
     });
 
@@ -296,10 +309,15 @@ export class StatisticsComponent implements OnInit {
   }
 
   setValueGraph1() {
-    this.value = [[this.scoreMoyenPetitesEntreprises,
-      this.scoreMoyenMoyennesEntreprises,
-      this.scoreMoyenGrandesEntreprises],
-      [this.scoreMoyenPetitesEntreprises,
+    this.value = [
+      [
+        this.scoreMoyenPetitesEntreprises,
+        this.scoreMoyenMoyennesEntreprises,
+        this.scoreMoyenGrandesEntreprises
+      ],
+
+      [
+        this.scoreMoyenPetitesEntreprises,
         this.scoreMoyenMoyennesEntreprises,
         this.scoreMoyenGrandesEntreprises]
     ];
@@ -319,7 +337,7 @@ export class StatisticsComponent implements OnInit {
     });
   }
 
-  includeMetier(metiers : string[], metier : string){
+  includeMetier(metiers: string[], metier: string) {
     var res = false;
     metiers.forEach((met) => {
       if (met == metier) {
